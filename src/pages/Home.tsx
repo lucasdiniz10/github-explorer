b7654aa0b6113af7d77fb396452ca270733b5ffc
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-04-18 11:37:50 -0300

    home title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import { Header } from "../components/Header";
 import { RepositoryList } from "../components/RepositoryList";
+import '../styles/home.scss'
 
 export function Home() {
   return (
     <>
       <Header />
-      <div className="home-container ">
+      <div className="home-container container">
         <h1>Explore repositórios
 no Github.</h1>
         <RepositoryList />
